ad7b94f61c8c5be7faa8e6cfdd6f1fd33fc92862
Roman Coropat <roman.koropat@gmail.com>
2023-11-27 13:13:47 +0200

    add: remote images config

--- src/app/components/Gallery/component.tsx
+++ src/app/components/Gallery/component.tsx
@@ -1,5 +1,6 @@
 import fetchImages from "@/lib/fetchImages";
 import type { ImagesResults } from "@/models/Images";
+import ImageContainer from "../ImageContainer/component";
 
 export default async function Gallery() {
     const url = 'https://api.pexels.com/v1/curated';
@@ -15,7 +16,7 @@ export default async function Gallery() {
     return (
         <section className="px-2 my-3 grid gap-2 grid-cols-gallery">
                 {images.photos.map(photo => (
-                    <div key={photo.id} className="h-64 bg-gray-200 rounded-xl"></div>
+                    <ImageContainer key={photo.id} photo={photo} />
                 ))}
         </section>
     )
